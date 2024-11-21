dc07fc2a197e7a55591d1ccb7511436e756518c3
Zipp <142827995+Zapak69@users.noreply.github.com>
2024-11-21 17:18:56 +0100

    Update clsi.bat

--- clsi.bat
+++ clsi.bat
@@ -1,6 +1,7 @@
 @echo off
-set "ver=1.0.3.2.8.2"
+set "ver=1.0.4.3.2.0"
 echo Commnad Line Script Import %ver%
+if /i "%1" == "-version" exit /b 0
 timeout /t 0 /nobreak >nul
 echo [92m[IMPORTING...][0m
 timeout /t 0 /nobreak >nul
@@ -13,7 +14,7 @@ if "%errorlevel%" == "0" (
 	pause >nul
 	exit
 )
-powershell -Command "if (Test-Connection -ComputerName github.com -Count 1 -Quiet) { exit 0 } else { exit 1 }"
+powershell -Command "if (Test-Connection -ComputerName google.com -Count 1 -Quiet) { exit 0 } else { exit 1 }"
 if "%errorlevel%" == "0" (
 	echo [90m[GITHUB RESPONDING...][0m
 	timeout /t 0 /nobreak >nul
@@ -47,69 +48,20 @@ if /i "%command%" == "$install gui-buttons" (
 	powershell.exe -Command "Write-Host '[100m[97mFunction installed to: [92m%systemroot%\System32[97m' -NoNewline"
 )
 
-:: APP INSTALL COMMANDS
-if /i "%command%" == "$app install chrome" (
-	PowerShell.exe -ExecutionPolicy Bypass -Command "irm 'https://dl.google.com/tag/s/appguid%3D%7B8A69D345-D564-463C-AFF1-A69D9E530F96%7D%26iid%3D%7B40D2092E-9681-5275-E300-7AF18C93F373%7D%26lang%3Dcs%26browser%3D4%26usagestats%3D1%26appname%3DGoogle%2520Chrome%26needsadmin%3Dprefers%26ap%3Dx64-statsdef_1%26installdataindex%3Dempty/update2/installers/ChromeSetup.exe' -UseBasicParsing -OutFile '%temp%\chrome_setup.exe'"
-	start %temp%\chrome_setup.exe
-)
-
 ::OTHER COMMANDS
 if /i "%command%" == "$clear" cls
 if /i "%command%" == "$exit" (
 	echo Thanks for using CLSI! Goodbye.
 	echo.
-	timeout /t 0 /nobreak >nul
 	exit
 )
-if /i "%command%" == "$" call :help
-if /i "%command%" == "$help" call :help
-
-
-::SETTING COMMANDS
-if /i "%command%" == "$check" (
-	powershell -Command "if (Test-Connection -ComputerName github.com -Count 1 -Quiet) { exit 0 } else { exit 1 }"
-	if "%errorlevel%" == "0" (
-		echo Github connection is working!
-	) else (
-		echo Failed to connect to github!
-	)
-)
-
-::HARDER COMMANDS
-echo %command% | find "$start" >nul
-if "%errorlevel%" == "0" (
-    call :start
-    set start=1
-)
+if /i "%command%" == "$version" echo CLSI VERSION: %ver%
 
 
 ::COMMAND NOT FOUND HANDLER
 echo.
-if not "%command%" == "$import" if not "%command%" == "$import base-gui" if not "%command%" == "$clear" if NOT "%command%" == "$install gui-buttons" if NOT "%command%" == "$check" if NOT "%command%" == "$app install chrome" if NOT "%command%" == "$" if NOT "%command%" == "$help" if NOT "%start%" == "1" (
+if not "%command%" == "$import" if not "%command%" == "$import base-gui" if not "%command%" == "$clear" if NOT "%command%" == "$install gui-buttons" (
     echo Unrecognized or incompleted command: "%command%"
 )
+
 goto :command
-
-:start
-for /f "tokens=1,2 delims= " %%a in ("%command%") do (
-    set start=%%b
-)
-start "" "%start%"
-exit /b 0
-
-:help
-echo [46m[CLSI HELP][0m
-echo.
-echo [92mCommand list:[0m
-echo $import - script importation
-echo $install - function installation
-echo $app - app operations
-echo $clear - clear console
-echo $exit - exit
-echo $check - conectivity check
-echo.
-echo [95mCommand addons:[0m
-echo $import base-gui - basic gui menu
-echo $install gui-buttons - Gui buttons installation
-echo $app install chrome - google chrome installation
-exit /b 0
